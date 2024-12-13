887970e4a106638a8d66676359dd040699865a21
Hoàng Văn Bảo <baonet003@gmail.com>
2024-12-13 16:49:30 +0700

    Locket_Ohoang7.js

--- Locket_Ohoang7.js
+++ Locket_Ohoang7.js
@@ -4,19 +4,19 @@ const mapping = {
   'Locket': ['Gold'] // Đảm bảo rằng Locket Gold được sử dụng đúng cách
 };
 
-// =========   Phần cố định  ========= //
-// =========  @Ohoang7 ========= // 
+// ========= Phần cố định ========= //
+// ========= @HoangVanBao ========= // 
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
 var obj = JSON.parse($response.body);
-obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+obj.Attention = "Chúc mừng bạn Hoàng Văn Bảo! Vui lòng không bán hoặc chia sẻ cho người khác!";
 
 // Tạo thông tin về gói Locket Gold
-var ohoang7 = {
+var hoangvanbao = {
   is_sandbox: false,
   ownership_type: "PURCHASED",
   billing_issues_detected_at: null,
   period_type: "normal",
-  expires_date: "2099-12-18T01:04:17Z",  // Đảm bảo ngày hết hạn là lâu dài
+  expires_date: "2099-12-18T01:04:17Z", // Ngày hết hạn lâu dài
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
   original_purchase_date: "2024-07-28T01:04:18Z",
@@ -24,10 +24,10 @@ var ohoang7 = {
   store: "app_store"
 };
 
-var vuong2023 = {
+var hvb_entitlement = {
   grace_period_expires_date: null,
   purchase_date: "2024-07-28T01:04:17Z",
-  product_identifier: "com.ohoang7.premium.yearly",
+  product_identifier: "com.hoangvanbao.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };
 
@@ -35,11 +35,11 @@ var vuong2023 = {
 const match = Object.keys(mapping).find(e => ua.includes(e));
 if (match) {
   let [e, s] = mapping[match];
-  s ? (vuong2023.product_identifier = s, obj.subscriber.subscriptions[s] = ohoang7) : obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7;
-  obj.subscriber.entitlements[e] = vuong2023; // Gán Locket Gold vào entitlements
+  s ? (hvb_entitlement.product_identifier = s, obj.subscriber.subscriptions[s] = hoangvanbao) : obj.subscriber.subscriptions["com.hoangvanbao.premium.yearly"] = hoangvanbao;
+  obj.subscriber.entitlements[e] = hvb_entitlement; // Gán Locket Gold vào entitlements
 } else {
-  obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7;
-  obj.subscriber.entitlements.Locket = vuong2023; // Đảm bảo Locket Gold được thêm vào
+  obj.subscriber.subscriptions["com.hoangvanbao.premium.yearly"] = hoangvanbao;
+  obj.subscriber.entitlements.Locket = hvb_entitlement; // Đảm bảo Locket Gold được thêm vào
 }
 
 $done({ body: JSON.stringify(obj) });
