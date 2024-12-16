1e3fc112b6252cbe3a47983c0b07443a23641b90
Hoàng Văn Bảo <baonet003@gmail.com>
2024-12-16 16:34:28 +0700

    Update deleteHeader.js

--- deleteHeader.js
+++ deleteHeader.js
@@ -9,8 +9,10 @@ function setHeaderValue(e, a, d) {
 // Lấy headers hiện tại từ request
 var modifiedHeaders = $request.headers;
 
-// Thay đổi giá trị của X-RevenueCat-ETag
-setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
+// Kiểm tra xem header "X-RevenueCat-ETag" có tồn tại không trước khi thay đổi
+if(modifiedHeaders["X-RevenueCat-ETag"]) {
+  setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
+}
 
 // Debug: In header đã sửa (tuỳ chọn)
 console.log("Modified Headers:", JSON.stringify(modifiedHeaders));
